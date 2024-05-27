230e7492b726dba80e4456f29016450b12227d68
Dmitrii Bulatov <bulati484@gmail.com>
2024-05-27 23:40:29 +0300

    next

--- service/token-service.js
+++ service/token-service.js
@@ -6,14 +6,16 @@ require("dotenv").config();
 
 class TokenService {
   generateToken(payload) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' });
-    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d'});
+    // const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' });
+    // const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d'});
+    const accessToken = jwt.sign(payload, value.JWT_ACCESS_SECRET, { expiresIn: '30m' });
+    const refreshToken = jwt.sign(payload, value.JWT_REFRESH_SECRET, { expiresIn: '30d'});
     return { accessToken, refreshToken };
   };
 
   validateAccessToken(token) {
     try {
-      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+      const userData = jwt.verify(token, value.JWT_ACCESS_SECRET);
       return userData;
     } catch(e) {
       return null;
@@ -22,7 +24,7 @@ class TokenService {
 
   validateRefreshToken(token) {
     try {
-      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+      const userData = jwt.verify(token, value.JWT_REFRESH_SECRET);
       return userData;
     } catch(e) {
       return null;
